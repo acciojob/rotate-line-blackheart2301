a61607a43e03e1232c121487583a2e9b3cdacab5
blackheart2301 <128833356+blackheart2301@users.noreply.github.com>
2023-05-29 17:44:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const container= document.getElementsByClassName("container")[0];
+
+function showElement(){
+	const div= document.createElement("div");
+	div.id="line";
+	container.append(div);
+}
